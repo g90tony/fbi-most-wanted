7f36c997399f9ba0a66b582146b188cc5353dbcf
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-11 16:58:00 +0300

    implemented queryClientProvider and ToasterProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainRouter from "./routes";
+import { Toaster } from "sonner";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="flex flex-col items-center justify-start w-screen h-screen p-0 m-0 bg-black text-primary">
-      <MainRouter />
+      <QueryClientProvider client={queryClient}>
+        <MainRouter />
+        <Toaster richColors />
+      </QueryClientProvider>
     </div>
   );
 }
