2a756d4ee41ee00bf4a56fa572c0dec54ddbed84
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-11 15:09:52 +0300

    added signUpPage and implemented signUpform

---
+++ client/src/pages/public/signUpPage.tsx
@@ -0,0 +1,11 @@
+import { SignUpForm } from "@/components/forms/signup";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-[80vh] flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex flex-col w-full max-w-sm items-center gap-6">
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
