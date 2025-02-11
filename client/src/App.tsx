78a7755e87e84c6b5c120b64cd5052ed88b2398a
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-11 19:11:13 +0300

    added sidebarProved sive container to wrap mainRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainRouter from "./routes";
 import { Toaster } from "sonner";
+import { SidebarProvider } from "./components/ui/sidebar";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
-    <div className="flex flex-col items-center justify-start w-screen h-screen p-0 m-0 bg-black text-primary">
-      <QueryClientProvider client={queryClient}>
-        <MainRouter />
+    <QueryClientProvider client={queryClient}>
+      <SidebarProvider>
+        <section className="flex flex-col items-center justify-start w-screen h-screen p-0 m-0 bg-black text-primary">
+          <MainRouter />
+        </section>
         <Toaster richColors />
-      </QueryClientProvider>
-    </div>
+      </SidebarProvider>
+    </QueryClientProvider>
   );
 }
 
