4f64870ebdad6ba7792803d7ff23cadfd4fda3d5
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-11 23:48:02 +0300

    implemented live api call

--- client/src/api/handleNewUserSignUp.ts
+++ client/src/api/handleNewUserSignUp.ts
@@ -1,3 +1,5 @@
+import axiosClient from "@/lib/axios/axiosClient";
+import isDevEnv from "@/lib/isDevEnv";
 import { signUpFormSchema, TSignUpFormSchema } from "@/types/formSchema";
 
 export default async function handleNewUserSignUp(form: TSignUpFormSchema) {
@@ -16,4 +18,25 @@ export default async function handleNewUserSignUp(form: TSignUpFormSchema) {
       "There was a problem submitting the details. Please check the inputs and try again."
     );
   }
+
+  let response: boolean | null = null;
+
+  try {
+    response = await new Promise((resolve, reject) => {
+      axiosClient
+        .get("/")
+        .then(() => {
+          resolve(true);
+        })
+        .catch((error: Error) => {
+          reject(error);
+        });
+    });
+  } catch (error: unknown) {
+    if (isDevEnv) console.error("There was an error creating the user", error);
+
+    throw new Error("There was an error creating the user");
+  }
+
+  return response;
 }
