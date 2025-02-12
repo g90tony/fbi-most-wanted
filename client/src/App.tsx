ac3db52125239d906da5304db5e5ca2e1e970aaf
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-12 14:04:58 +0300

    added redux store provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,23 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainRouter from "./routes";
 import { Toaster } from "sonner";
 import { SidebarProvider } from "./components/ui/sidebar";
+import store from "./state/store";
+import { Provider } from "react-redux";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <SidebarProvider>
-        <section className="flex flex-col items-center justify-start w-screen h-screen p-0 m-0 bg-black text-primary">
-          <MainRouter />
-        </section>
-        <Toaster richColors />
-      </SidebarProvider>
-    </QueryClientProvider>
+    <section className="flex flex-col items-center justify-start w-screen h-screen p-0 m-0 !bg-black text-primary">
+      <QueryClientProvider client={queryClient}>
+        <SidebarProvider className="bg-black">
+          <Provider store={store}>
+            <MainRouter />
+          </Provider>
+          <Toaster richColors />
+        </SidebarProvider>
+      </QueryClientProvider>
+    </section>
   );
 }
 
