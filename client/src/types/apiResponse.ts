4655b276f78b355f3c837089c0c4afdf03dd8cda
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-12 14:03:21 +0300

    added TDashboardListResponse

--- client/src/types/apiResponse.ts
+++ client/src/types/apiResponse.ts
@@ -1,6 +1,10 @@
+import { TWantedPersonMeta } from "./types";
+
 export type TUserSignInResponse = {
   id: number;
   name: string;
   email: string;
   sessionToken: string;
 };
+
+export type TDashboardListResponse = TWantedPersonMeta[];
