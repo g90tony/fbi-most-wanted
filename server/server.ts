1582c2ca02a7b791419e3d3300cc86617553316a
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-12 03:08:40 +0300

    implemented cor

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,29 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 
+const cors = require("cors");
 const authRouter = require("./routes/auth");
 const watchListRouter = require("./routes/watchList");
 
 const server: Express = express();
 
+var allowlist = ["*"];
+
+var corsOptionsDelegate = function (req: any, callback: any) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
 dotenv.config();
+
+server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(cors(corsOptionsDelegate));
 
 const port = process.env.SERVER_PORT || 4002;
 
