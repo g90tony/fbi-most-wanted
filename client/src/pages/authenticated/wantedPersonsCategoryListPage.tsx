cd0176c3cb05503eac8942336dcbd212b46578e2
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-12 14:02:23 +0300

    completed wantedPersonsCategoryListPage

---
+++ client/src/pages/authenticated/wantedPersonsCategoryListPage.tsx
@@ -0,0 +1,9 @@
+import CategoriesCategoryWantedList from "@/components/custom/categoryWantedList";
+
+export default function WantedPersonsCategoryListPage() {
+  return (
+    <section className="flex flex-col w-full min-h-[90vh] gap-4 overflow-y-auto p-0 m-0">
+      <CategoriesCategoryWantedList />
+    </section>
+  );
+}
