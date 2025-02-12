9a2d8e4317988d519ca8cb6d2f60adbf04f0157c
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-12 14:00:39 +0300

    completed dashboard

---
+++ client/src/pages/authenticated/dashboardPage.tsx
@@ -0,0 +1,9 @@
+import DashboardWantedList from "@/components/custom/dashboardWantedList";
+
+export default function DashboardPage() {
+  return (
+    <section className="flex flex-col w-full min-h-[90vh] gap-4 overflow-y-auto p-0 m-0">
+      <DashboardWantedList />
+    </section>
+  );
+}
