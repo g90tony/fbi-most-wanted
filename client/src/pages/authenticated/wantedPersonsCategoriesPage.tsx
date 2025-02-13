4273f809227abadd7ae2373cc5dcaa5c8d4672a5
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-13 18:09:59 +0300

    implemented renamed categoriesList

--- client/src/pages/authenticated/wantedPersonsCategoriesPage.tsx
+++ client/src/pages/authenticated/wantedPersonsCategoriesPage.tsx
@@ -1,9 +1,9 @@
-import CategoriesCategoryList from "@/components/custom/categoriesWantedList ";
+import CategoriesList from "@/components/custom/wantedCategoriesList ";
 
 export default function WantedPersonsCategoriesPage() {
   return (
     <section className="flex flex-col w-full min-h-[90vh] gap-4 overflow-y-auto">
-      <CategoriesCategoryList />
+      <CategoriesList />
     </section>
   );
 }
