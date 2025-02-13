bef810e5a1e9397482f61703902545a70edf401b
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-13 23:14:11 +0300

    added type prop

---
+++ client/src/pages/authenticated/userWantedList.tsx
@@ -0,0 +1,9 @@
+import WantedList from "@/components/custom/wantedList";
+
+export default function UserWantedList() {
+  return (
+    <section className="flex flex-col w-full min-h-[90vh] gap-4 overflow-y-auto">
+      <WantedList />
+    </section>
+  );
+}
--- client/src/pages/authenticated/wantedPersonsCategoryListPage.tsx
+++ client/src/pages/authenticated/wantedPersonsCategoryListPage.tsx
@@ -3,7 +3,7 @@ import WantedList from "@/components/custom/wantedList";
 export default function WantedPersonsCategoryListPage() {
   return (
     <section className="flex flex-col w-full min-h-[90vh] gap-4 overflow-y-auto p-0 m-0">
-      <WantedList />
+      <WantedList type="categorized" />
     </section>
   );
 }
