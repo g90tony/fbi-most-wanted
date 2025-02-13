cc655627e5850b170c3009a1387c115c4d067547
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-13 17:54:47 +0300

    deleted duplicate wanted lists for centralized component

--- client/src/components/custom/wantedList.tsx
+++ client/src/components/custom/wantedList.tsx
@@ -1,43 +1,46 @@
-import handleDashboardGetInitialList from "@/api/handleDashboardGetInitialList";
+import handleGetWantedList from "@/api/handleGetWantedList";
 import isDevEnv from "@/lib/isDevEnv";
 import { AuthState } from "@/state/slices/authStateSlice";
 import { TDashboardListResponse } from "@/types/apiResponse";
 import { TAuthState } from "@/types/state";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { toast } from "sonner";
 import GlobalLoader from "./globalLoader";
 import { cn } from "@/lib/utils";
 import { Card, CardContent, CardHeader } from "../ui/card";
-import { Plus } from "lucide-react";
-import { TWantedPersonMeta } from "@/types/types";
-import { Button } from "../ui/button";
 
-export default function DashboardWantedList() {
+export default function WantedList() {
   const authState: TAuthState = useSelector(AuthState);
   const router = useNavigate();
 
+  const observerRef = useRef<IntersectionObserver>(null);
+
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   const [wantedList, setWantedList] = useState<TDashboardListResponse>([]);
 
   function handleViewWantedPerson(personUID: string) {
-    router(`/wanted-persons/${personUID}`);
+    router(`/wanted-persons/${personUID}`, {
+      state: { trigger: "user" },
+    });
   }
 
   const handleFetchData = useCallback(
     async function () {
       if (authState.isAuthenticated && authState.authenticatedUser !== null) {
-        setIsLoading(true);
+        if (currentPage === 1) setIsLoading(true);
 
         try {
-          const response = await handleDashboardGetInitialList(
-            authState.authenticatedUser.token
+          const response = await handleGetWantedList(
+            authState.authenticatedUser.token,
+            currentPage
           );
 
           if (response !== undefined && response.status === "success") {
-            setWantedList(response.data);
+            setWantedList((prevState) => [...prevState, ...response.data]);
             setIsLoading(false);
           }
         } catch (error) {
@@ -53,49 +56,43 @@ export default function DashboardWantedList() {
           );
         }
       } else {
-        router("/signup");
+        router("/sign-up", {
+          state: { trigger: "user" },
+        });
       }
     },
-    [authState.authenticatedUser, authState.isAuthenticated, router]
+    [
+      authState.authenticatedUser,
+      authState.isAuthenticated,
+      currentPage,
+      router,
+    ]
   );
 
-  const handleAddToPersonalList = useCallback(
-    async function (selectedPerson: TWantedPersonMeta) {
-      if (authState.isAuthenticated && authState.authenticatedUser !== null) {
-        try {
-          const response = await handleDashboardGetInitialList(
-            authState.authenticatedUser.token
-          );
+  const lastListElementRef = useCallback((node: HTMLDivElement) => {
+    if (observerRef.current) observerRef.current.disconnect();
 
-          if (response) {
-            toast.success(
-              `${selectedPerson.title} wass added successfully to your watch list`,
-              {
-                id: "add-to-watchlist",
-              }
-            );
-          }
-        } catch (error) {
-          if (isDevEnv) {
-            console.error("FETCH_DASHBOARD_FAILURE", error);
-          }
-          toast.error(
-            "There was a problem loading the data. Please try again later",
-            {
-              id: "add-to-watchlist",
-            }
-          );
-        }
-      } else {
-        router("/signup");
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setCurrentPage((prevPage) => prevPage + 1); // trigger loading of new posts by chaging page no
       }
-    },
-    [authState.authenticatedUser, authState.isAuthenticated, router]
-  );
+    });
+
+    if (node) observerRef.current.observe(node);
+  }, []);
+
+  useMemo(() => {
+    if (currentPage > 1) {
+      handleFetchData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
 
   useEffect(() => {
-    handleFetchData();
-  }, [handleFetchData]);
+    if (currentPage === 1) {
+      handleFetchData();
+    }
+  }, [currentPage, handleFetchData]);
 
   return (
     <div
@@ -111,19 +108,15 @@ export default function DashboardWantedList() {
         wantedList.length > 0 &&
         wantedList.map((person, index: number) => (
           <Card
+            ref={
+              index === wantedList.length - 1 ? lastListElementRef : undefined
+            }
             key={index}
             onClick={() => handleViewWantedPerson(person.uid)}
             className="grid col-span-1 lg:col-span-1 rounded-xl justify-items-stretch text-center content-start w-full h-full max-h-[550px] border-0 mb-4 bg-zinc-950 cursor-pointer"
           >
-            <CardHeader className="flex flex-row justify-end items-center w-full h-auto">
-              <Button
-                onClick={() => handleAddToPersonalList(person)}
-                className="bg-blue-400 text-sm font-bold rounded-full border-0 w-auto h-8"
-              >
-                Add to WatchList <Plus />
-              </Button>
-            </CardHeader>
-            <CardContent className="flex flex-col items-center justify-start h-[400px]">
+            <CardHeader className="flex flex-row justify-end items-center w-full h-auto"></CardHeader>
+            <CardContent className="flex flex-col items-center justify-start min-h-[400px]">
               <div className="flex flex-row w-full h-[350px] mb-4">
                 <img
                   src={person.image}
