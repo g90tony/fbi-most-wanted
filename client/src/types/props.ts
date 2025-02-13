89436b21731be0e57a972ccd6230b705796aa82f
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-13 23:15:14 +0300

    added WantedListItemProps

--- client/src/types/props.ts
+++ client/src/types/props.ts
@@ -1,3 +1,7 @@
+import { Ref } from "react";
+import { TWantedPersonMeta } from "./types";
+import { TWantedListRenderTypes } from "./state";
+
 export type TGlobalStatItemProps = {
   label: string;
   statistic: string;
@@ -6,4 +10,14 @@ export type TGlobalStatItemProps = {
 export type TGlobalLoaderProps = {
   message: string;
   type: "card" | "page";
+};
+
+export type WantedListItemProps = {
+  handleViewWantedPerson: (personUid: string) => void;
+  person: TWantedPersonMeta;
+  lastElementRef: Ref<HTMLDivElement> | undefined;
+};
+
+export type WantedListProps = {
+  type: TWantedListRenderTypes;
 };
