5c6e4b5e1cbf2c7133ce565c6d20ab9f9838b98b
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-13 23:12:49 +0300

    refactored unified component and implemented wantedListItem

--- client/src/components/custom/wantedList.tsx
+++ client/src/components/custom/wantedList.tsx
@@ -1,52 +1,80 @@
 import handleGetWantedList from "@/api/handleGetWantedList";
 import isDevEnv from "@/lib/isDevEnv";
 import { AuthState } from "@/state/slices/authStateSlice";
-import { TWantedListResponse } from "@/types/apiResponse";
-import { TAuthState } from "@/types/state";
+import { TAuthState, TWantedListState } from "@/types/state";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
 import { toast } from "sonner";
 import GlobalLoader from "./globalLoader";
 import { cn } from "@/lib/utils";
-import { Card, CardContent, CardHeader } from "../ui/card";
+import {
+  LOAD_LIST_DATA,
+  LOAD_NEXT_PAGE,
+  LOAD_NEXT_PAGE_LIST_DATA,
+  WantedListState,
+} from "@/state/slices/wantedListSlice";
+import WantedListItem from "./wantedListItem";
+import GlobalEmptyPlaceholder from "./globalEmptyPlaceholder";
+import { WantedListProps } from "@/types/props";
 
-export default function WantedList() {
+export default function WantedList({ type = "normal" }: WantedListProps) {
   const authState: TAuthState = useSelector(AuthState);
+  const wantedListState: TWantedListState = useSelector(WantedListState);
+
   const router = useNavigate();
+  const dispatch = useDispatch();
+  const params = useParams();
 
   const observerRef = useRef<IntersectionObserver>(null);
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [isEmpty, setIsEmpty] = useState<boolean>(true);
 
-  const [wantedList, setWantedList] = useState<TWantedListResponse>([]);
+  const handleViewWantedPerson = useCallback(
+    function (personUID: string) {
+      router(`/wanted-persons/${personUID}`, {
+        state: { trigger: "user" },
+      });
+    },
+    [router]
+  );
 
-  function handleViewWantedPerson(personUID: string) {
-    router(`/wanted-persons/${personUID}`, {
-      state: { trigger: "user" },
-    });
-  }
-
-  const handleFetchData = useCallback(
+  const handleFetchMyWantedListData = useCallback(
     async function () {
       if (authState.isAuthenticated && authState.authenticatedUser !== null) {
-        if (currentPage === 1) setIsLoading(true);
+        setIsEmpty(false);
+        if (
+          wantedListState.currentPage === 1 &&
+          wantedListState.myWantedList.length === 0
+        )
+          setIsLoading(true);
 
         try {
           const response = await handleGetWantedList(
             authState.authenticatedUser.token,
-            currentPage
+            wantedListState.currentPage
           );
 
           if (response !== undefined && response.status === "success") {
-            setWantedList((prevState) => [...prevState, ...response.data]);
+            if (wantedListState.currentPage === 1) {
+              dispatch(LOAD_LIST_DATA({ listType: type, list: response.data }));
+            } else {
+              dispatch(
+                LOAD_NEXT_PAGE_LIST_DATA({
+                  listType: type,
+                  list: response.data,
+                })
+              );
+            }
             setIsLoading(false);
           }
         } catch (error) {
           if (isDevEnv) {
             console.error("FETCH_DASHBOARD_FAILURE", error);
           }
+          setIsLoading(false);
+          setIsEmpty(true);
 
           toast.error(
             "There was a problem loading the data. Please try again later",
@@ -61,84 +89,388 @@ export default function WantedList() {
         });
       }
     },
-    [
-      authState.authenticatedUser,
-      authState.isAuthenticated,
-      currentPage,
-      router,
-    ]
+    [authState, wantedListState, dispatch, router, type]
   );
 
-  const lastListElementRef = useCallback((node: HTMLDivElement) => {
-    if (observerRef.current) observerRef.current.disconnect();
+  const handleFetchCategorizedWantedListData = useCallback(
+    async function () {
+      if (authState.isAuthenticated && authState.authenticatedUser !== null) {
+        setIsEmpty(false);
+        if (
+          wantedListState.currentPage === 1 &&
+          wantedListState.categorizedWantedList.length === 0
+        )
+          setIsLoading(true);
 
-    observerRef.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        setCurrentPage((prevPage) => prevPage + 1); // trigger loading of new posts by chaging page no
+        try {
+          const response = await handleGetWantedList(
+            authState.authenticatedUser.token,
+            wantedListState.currentPage
+          );
+
+          if (response !== undefined && response.status === "success") {
+            if (wantedListState.currentPage === 1) {
+              dispatch(LOAD_LIST_DATA({ listType: type, list: response.data }));
+            } else {
+              dispatch(
+                LOAD_NEXT_PAGE_LIST_DATA({
+                  listType: type,
+                  list: response.data,
+                })
+              );
+            }
+            setIsLoading(false);
+          }
+        } catch (error) {
+          if (isDevEnv) {
+            console.error("FETCH_DASHBOARD_FAILURE", error);
+          }
+          setIsLoading(false);
+          setIsEmpty(false);
+
+          toast.error(
+            "There was a problem loading the data. Please try again later",
+            {
+              id: "fetch-dashboard-statistics",
+            }
+          );
+        }
+      } else {
+        router("/sign-up", {
+          state: { trigger: "user" },
+        });
       }
-    });
+    },
+    [authState, wantedListState, dispatch, router, type]
+  );
 
-    if (node) observerRef.current.observe(node);
-  }, []);
+  const handleFetchFilteredListData = useCallback(
+    async function () {
+      if (authState.isAuthenticated && authState.authenticatedUser !== null) {
+        setIsEmpty(false);
+        if (
+          wantedListState.currentPage === 1 &&
+          wantedListState.filteredWantedList.length === 0
+        )
+          setIsLoading(true);
+
+        try {
+          const response = await handleGetWantedList(
+            authState.authenticatedUser.token,
+            wantedListState.currentPage
+          );
+
+          if (response !== undefined && response.status === "success") {
+            if (wantedListState.currentPage === 1) {
+              dispatch(LOAD_LIST_DATA({ listType: type, list: response.data }));
+            } else {
+              dispatch(
+                LOAD_NEXT_PAGE_LIST_DATA({
+                  listType: type,
+                  list: response.data,
+                })
+              );
+            }
+            setIsLoading(false);
+          }
+        } catch (error) {
+          if (isDevEnv) {
+            console.error("FETCH_DASHBOARD_FAILURE", error);
+          }
+          setIsLoading(false);
+          setIsEmpty(false);
+
+          toast.error(
+            "There was a problem loading the data. Please try again later",
+            {
+              id: "fetch-dashboard-statistics",
+            }
+          );
+        }
+      } else {
+        router("/sign-up", {
+          state: { trigger: "user" },
+        });
+      }
+    },
+    [authState, wantedListState, dispatch, router, type]
+  );
+
+  const handleFetchWantedListData = useCallback(
+    async function () {
+      if (authState.isAuthenticated && authState.authenticatedUser !== null) {
+        setIsEmpty(false);
+        if (
+          wantedListState.currentPage === 1 &&
+          wantedListState.wantedList.length === 0
+        )
+          setIsLoading(true);
+
+        try {
+          const response = await handleGetWantedList(
+            authState.authenticatedUser.token,
+            wantedListState.currentPage
+          );
+
+          if (response !== undefined && response.status === "success") {
+            if (wantedListState.currentPage === 1) {
+              dispatch(LOAD_LIST_DATA({ listType: type, list: response.data }));
+            } else {
+              dispatch(
+                LOAD_NEXT_PAGE_LIST_DATA({
+                  listType: type,
+                  list: response.data,
+                })
+              );
+            }
+            setIsLoading(false);
+          }
+        } catch (error) {
+          if (isDevEnv) {
+            console.error("FETCH_DASHBOARD_FAILURE", error);
+          }
+          setIsLoading(false);
+          setIsEmpty(false);
+
+          toast.error(
+            "There was a problem loading the data. Please try again later",
+            {
+              id: "fetch-dashboard-statistics",
+            }
+          );
+        }
+      } else {
+        router("/sign-up", {
+          state: { trigger: "user" },
+        });
+      }
+    },
+    [authState, wantedListState, dispatch, type, router]
+  );
+
+  const lastListElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (observerRef.current) observerRef.current.disconnect();
+
+      observerRef.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          dispatch(LOAD_NEXT_PAGE());
+        }
+      });
+
+      if (node) observerRef.current.observe(node);
+    },
+    [dispatch]
+  );
+
+  const handleRenderList = useCallback(() => {
+    switch (type) {
+      case "filtered":
+        return wantedListState.filteredWantedList.length === 0 ? (
+          <GlobalEmptyPlaceholder
+            type="page"
+            message={`
+              <p style="font-weight:700">
+                There was a problem loading the queried data
+              </p> <br/>
+              <p style="color:#fff;font-size:0.7rem;margin-top:2px">
+                  Please reload the page.
+              </p> 
+           `}
+          />
+        ) : (
+          <>
+            {wantedListState.filteredWantedList.map((person, index: number) => (
+              <WantedListItem
+                lastElementRef={
+                  index === wantedListState.filteredWantedList.length - 1
+                    ? lastListElementRef
+                    : undefined
+                }
+                handleViewWantedPerson={handleViewWantedPerson}
+                person={person}
+                key={index}
+              />
+            ))}
+          </>
+        );
+
+      case "categorized":
+        return wantedListState.categorizedWantedList.length === 0 ? (
+          <GlobalEmptyPlaceholder
+            type="page"
+            message={`
+              <p style="font-weight:700">
+                There was a problem loading the wanted persons categorized under ${
+                  params.category ?? "--"
+                }
+              </p> <br/>
+              <p style="color:#fff;font-size:0.7rem;margin-top:2px">
+                  Please reload the page.
+              </p> 
+           `}
+          />
+        ) : (
+          <>
+            {wantedListState.wantedList.map((person, index: number) => (
+              <WantedListItem
+                lastElementRef={
+                  index === wantedListState.categorizedWantedList.length - 1
+                    ? lastListElementRef
+                    : undefined
+                }
+                handleViewWantedPerson={handleViewWantedPerson}
+                person={person}
+                key={index}
+              />
+            ))}
+          </>
+        );
+
+      case "personal":
+        return wantedListState.myWantedList.length === 0 ? (
+          <GlobalEmptyPlaceholder
+            type="page"
+            message={`
+              <p style="font-weight:700">
+                There was a problem loading the your watch list data.
+              </p> <br/>
+              <p style="color:#fff;font-size:0.7rem;margin-top:2px">
+                  Please reload the page.
+              </p> 
+           `}
+          />
+        ) : (
+          <>
+            {wantedListState.myWantedList.map((person, index: number) => (
+              <WantedListItem
+                lastElementRef={
+                  index === wantedListState.filteredWantedList.length - 1
+                    ? lastListElementRef
+                    : undefined
+                }
+                handleViewWantedPerson={handleViewWantedPerson}
+                person={person}
+                key={index}
+              />
+            ))}
+          </>
+        );
+
+      case "normal":
+        return wantedListState.wantedList.length === 0 ? (
+          <GlobalEmptyPlaceholder
+            type="page"
+            message={`
+              <p style="font-weight:700">
+                There was a problem loading the wanted persons data.
+              </p> <br/>
+              <p style="color:#fff;font-size:0.7rem;margin-top:2px">
+                  Please reload the page.
+              </p> 
+           `}
+          />
+        ) : (
+          wantedListState.wantedList.map((person, index: number) => (
+            <WantedListItem
+              lastElementRef={
+                index === wantedListState.wantedList.length - 1
+                  ? lastListElementRef
+                  : undefined
+              }
+              handleViewWantedPerson={handleViewWantedPerson}
+              person={person}
+              key={index}
+            />
+          ))
+        );
+
+      default:
+        return (
+          <GlobalEmptyPlaceholder
+            type="page"
+            message={`
+              <p style="font-weight:700">
+                There was a problem loading the page.
+              </p> <br/>
+              <p style="color:#fff;font-size:0.7rem;margin-top:2px">
+                  Please reload the page or try again later.
+              </p> 
+           `}
+          />
+        );
+    }
+  }, [
+    handleViewWantedPerson,
+    lastListElementRef,
+    params.category,
+    type,
+    wantedListState.categorizedWantedList.length,
+    wantedListState.filteredWantedList,
+    wantedListState.myWantedList,
+    wantedListState.wantedList,
+  ]);
 
   useMemo(() => {
-    if (currentPage > 1) {
-      handleFetchData();
+    if (wantedListState.currentPage > 1) {
+      switch (wantedListState.listType) {
+        case "filtered":
+          handleFetchFilteredListData();
+
+          break;
+        case "categorized":
+          handleFetchCategorizedWantedListData();
+
+          break;
+        case "personal":
+          handleFetchMyWantedListData();
+
+          break;
+        case "normal":
+          handleFetchWantedListData();
+          break;
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
+  }, [wantedListState.currentPage]);
 
   useEffect(() => {
-    if (currentPage === 1) {
-      handleFetchData();
+    if (wantedListState.currentPage === 1) {
+      switch (type) {
+        case "filtered":
+          handleFetchFilteredListData();
+
+          break;
+        case "categorized":
+          handleFetchCategorizedWantedListData();
+          break;
+        case "personal":
+          handleFetchMyWantedListData();
+
+          break;
+        case "normal":
+          handleFetchWantedListData();
+          break;
+        default:
+          setIsLoading(false);
+          break;
+      }
     }
-  }, [currentPage, handleFetchData]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div
       className={cn(
-        isLoading
+        isLoading || isEmpty
           ? "flex flex-row justify-center items-start w-full h-full min-h-[100px] bg-zinc-950 rounded-lg"
           : "grid grid-cols-1 lg:grid-cols-3 gap-4 w-full h-full rounded-lg"
       )}
     >
       {isLoading ? (
-        <GlobalLoader message="Loading Most Wonted List" type="card" />
+        <GlobalLoader message="Loading Page Data" type="card" />
       ) : (
-        wantedList.length > 0 &&
-        wantedList.map((person, index: number) => (
-          <Card
-            ref={
-              index === wantedList.length - 1 ? lastListElementRef : undefined
-            }
-            key={index}
-            onClick={() => handleViewWantedPerson(person.uid)}
-            className="grid col-span-1 lg:col-span-1 rounded-xl justify-items-stretch text-center content-start w-full h-full max-h-[550px] border-0 mb-4 bg-zinc-950 cursor-pointer"
-          >
-            <CardHeader className="flex flex-row justify-end items-center w-full h-auto"></CardHeader>
-            <CardContent className="flex flex-col items-center justify-start min-h-[400px]">
-              <div className="flex flex-row w-full h-[350px] mb-4">
-                <img
-                  src={person.image}
-                  alt={person.title}
-                  className="w-full h-full object-cover rounded-xl"
-                />
-              </div>
-              <p className="block w-full text-md text-blue-400 text-start font-bold">
-                {String(person.title).split("-")[0]}
-              </p>
-              <p className="block w-full text-sm text-white text-start font-semibold">
-                Nationality: {person.nationality ? person.nationality : "--"}
-              </p>
-              <p className="block w-full text-sm text-white text-start font-semibold">
-                Published on:{" "}
-                {person.publication
-                  ? new Date(person.publication).toDateString()
-                  : "--"}
-              </p>
-            </CardContent>
-          </Card>
-        ))
+        handleRenderList()
       )}
     </div>
   );
