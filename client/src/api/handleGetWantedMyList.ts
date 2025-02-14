9c5a8e07d40d6fa003214e57fe3a782d15eb9e30
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-14 12:35:38 +0300

    added handleGetWantedMyList endpoint

---
+++ client/src/api/handleGetWantedMyList.ts
@@ -0,0 +1,28 @@
+import { TWantedListResponse } from "@/types/apiResponse";
+import axios, { AxiosResponse } from "axios";
+
+export default async function handleGetWantedMyList(
+  token: string,
+  page: number
+) {
+  let response: AxiosResponse | null = null;
+
+  try {
+    response = await axios({
+      method: "GET",
+      url: `${import.meta.env.VITE_API_URL}my-list/list/${page}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    throw new Error({ ...(error as Error) }.message);
+  }
+
+  if (response !== null && response.status === 200) {
+    return {
+      status: "success",
+      data: response.data as TWantedListResponse,
+    };
+  }
+}
