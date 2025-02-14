284df3da5f13847d2742a159fa9bbde81e7dc63f
Caleb Tony Mbugua <calebmbugua@gmail.com>
2025-02-14 13:54:29 +0300

    added handlePostWantedMyListNewPerson

---
+++ client/src/api/handlePostWantedMyListNewPerson.ts
@@ -0,0 +1,36 @@
+import { TWantedListResponse } from "@/types/apiResponse";
+import axios from "axios";
+
+export default async function handlePostWantedMyListNewPerson(
+  token: string,
+  personUid: string
+) {
+  let response: TWantedListResponse | null = null;
+
+  try {
+    response = await new Promise((resolve, reject) => {
+      axios({
+        method: "post",
+        url: `${import.meta.env.VITE_API_URL}my-list/add-person`,
+        data: personUid,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res: unknown) => {
+          resolve(res as TWantedListResponse);
+        })
+        .catch((error: Error) => {
+          reject(error);
+        });
+    });
+  } catch (error: unknown) {
+    const err: Error = error as Error;
+
+    throw new Error(err.message as string);
+  }
+
+  if (response !== null) {
+    return true;
+  }
+}
